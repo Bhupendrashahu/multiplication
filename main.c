20537bcbb4bae6b03b63ecd4b4b3b6203aa24f84
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-04-08 10:32:41 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int first,secand,multipal;
+    printf("Enter the two no for multiple \n");
+    scanf("%d%d",&first,&secand);
+    multipal=first*secand;
+    printf("the multiple of two no is %d",multipal);
+    getch();
+}
